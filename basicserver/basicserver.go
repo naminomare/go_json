2763a09d2290f9e3dfc58cad29c171fc0bb3365a
naminomare <hiroaki.gohara@gmail.com>
2016-06-22 11:42:54 +0900

    簡易httpsサーバ基本
    簡易httpsサーバのテンプレ

---
+++ basicserver/basicserver.go
@@ -0,0 +1,109 @@
+package basicserver
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type ResponseFunc func(wp *http.ResponseWriter, rp *http.Request)
+
+type Page struct {
+	Title string
+}
+
+func DefaultResponseFunc(wp *http.ResponseWriter, rp *http.Request) {
+	fmt.Println("DEFAULT")
+
+	files, err := ioutil.ReadDir("static_res")
+	file_arr_str := ""
+	for i := 0; i < len(files); i++ {
+		file_arr_str += "\"" + files[i].Name() + "\","
+	}
+
+	page := Page{"DefaultPage"}
+	tmpl, err := template.ParseFiles("default.html")
+	if err != nil {
+		panic(err)
+	}
+
+	err = tmpl.Execute(*wp, page)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func SetStaticResponseFunc(wp *http.ResponseWriter, rp *http.Request) {
+	fname := rp.URL.Path[1:len(rp.URL.Path)]
+	if strings.HasSuffix(rp.URL.Path, ".css") {
+		(*wp).Header().Set("Content-Type", "text/css")
+	} else if strings.HasSuffix(rp.URL.Path, ".js") {
+		(*wp).Header().Set("Content-Type", "text/javascript")
+	} else if strings.HasSuffix(rp.URL.Path, ".ts") {
+		(*wp).Header().Set("Content-Type", "application/octet-stream")
+	}
+
+	fp, err := os.Open(fname)
+	if err != nil {
+		panic(err)
+	}
+	defer fp.Close()
+
+	buf := make([]byte, 1024*1024*10)
+	for {
+		n, _ := fp.Read(buf)
+		if n == 0 {
+			break
+		}
+		(*wp).Write(buf[:n])
+	}
+}
+func CheckAuth(r *http.Request, inputusername string, inputpassword string) bool {
+	username, password, ok := r.BasicAuth()
+	if ok == false {
+		return false
+	}
+	return username == inputusername && password == inputpassword
+}
+
+func ViewHandler(
+	w http.ResponseWriter,
+	r *http.Request,
+	checkAuthFunc func(*http.Request) bool,
+	authFunc func(http.ResponseWriter),
+	responseFuncMap map[string]ResponseFunc,
+	defaultFunc ResponseFunc,
+) {
+	if checkAuthFunc(r) == false {
+		authFunc(w)
+		return
+	}
+	//	r.ParseForm()
+
+	url_bytes := []byte(r.URL.Path)
+	match_flag := false
+	for k, f := range responseFuncMap {
+		matched, err := regexp.Match(k, url_bytes)
+		if err != nil {
+			panic(err)
+		}
+		if matched {
+			// debug print
+			fmt.Println("match")
+			fmt.Println(k)
+			fmt.Println(r.URL.Path)
+
+			// url to function
+			f(&w, r)
+			match_flag = true
+			break
+		}
+	}
+	if !match_flag {
+		defaultFunc(&w, r)
+	}
+}
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"./basicserver"
+)
+
+const (
+	cUSERNAME = "user"
+	cPASSWORD = "password"
+	cPORT     = "20000"
+)
+
+var responsefuncmap = map[string]basicserver.ResponseFunc{
+	"^/static_res": basicserver.SetStaticResponseFunc,
+}
+
+func authFunc(w http.ResponseWriter) {
+	w.Header().Set("WWW-Authenticate", "Basic realm='input your id and password")
+	w.WriteHeader(401)
+	w.Write([]byte("401 Unauthorized\n"))
+}
+
+func checkAuthFunc(r *http.Request) bool {
+	return basicserver.CheckAuth(r, cUSERNAME, cPASSWORD)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	basicserver.ViewHandler(
+		w,
+		r,
+		checkAuthFunc,
+		authFunc,
+		responsefuncmap,
+		basicserver.DefaultResponseFunc,
+	)
+}
+
+func main() {
+	fmt.Println("Hello World.")
+
+	http.HandleFunc("/", viewHandler)
+	http.ListenAndServeTLS(":"+cPORT, "cert.pem", "key.pem", nil)
+}
